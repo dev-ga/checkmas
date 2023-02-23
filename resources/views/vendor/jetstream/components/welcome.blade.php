08f290f83b5962d3243d2bb74f6bd8bcac52873e
STARKMEDIOS DEVELOPER <starkmediosdeveloper@Mac-mini-de-JHONNY.local>
2023-02-23 15:43:32 -0400

    cambios

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -6,13 +6,13 @@
     <div class="border border-gray-200 rounded-lg shadow-md px-4">
 
         {{-- Datos Orden de trabajo --}}
-        <div class="grid grid-cols-1 md:grid-cols-5 gap-4 mb-4 mt-8">
+        <div class="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4 mt-8">
             <div class="p-2">
                 <label class="opacity-60 mb-1 block text-sm font-medium text-italblue">@lang('messages.label.fechaInicio')</label>
                 <x-datetime-picker wire:model="fechaInicio" />
             </div>
             <div class="p-2">
-                <label class="opacity-60 mb-1 block text-sm font-medium text-italblue">@lang('messages.label.idEquipo')</label>
+                <label class="opacity-60 mb-1 block text-sm font-medium text-italblue">@lang('messages.label.tecnico')</label>
                 <x-native-select wire:model="tecnico">
                     <option></option>
                     <option value="110V">110V</option>
@@ -29,14 +29,6 @@
                     <option value="220V">220V</option>
                     <option value="380V">380V</option>
                     <option value="440V">440V</option>
-                </x-native-select>
-            </div>
-            <div class="p-2">
-                <label class="opacity-60 mb-1 block text-sm font-medium text-italblue">@lang('messages.label.tipoMan')</label>
-                <x-native-select wire:model="tecnico">
-                    <option></option>
-                    <option value="preventivo">Preventivo</option>
-                    <option value="correctivo">Correctivo</option>
                 </x-native-select>
             </div>
             <div class="p-2">
@@ -49,34 +41,33 @@
             </div>
         </div>
         
-        {{-- Mantenimiento preventivo --}}
+        {{-- Descripcion del equipo --}}
         <div class="border border-gray-200 rounded-lg shadow-md px-4">
-            {{-- Title Compresor --}}
             <div class="p-2">
                 <label class="text-xs font-extrabold text-black drop-shadow-lg">@lang('messages.label.dte')</label>
             </div>
-            {{-- Form Cmpresar --}}
-            <div class="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4">
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
                 <div class="p-2">
-                    <label class="opacity-60 mb-1 block text-sm font-medium text-italblue">@lang('messages.label.tipo')</label>
-                    <x-native-select wire:model="voltaje">
-                        <option></option>
-                        <option value="scroll">@lang('messages.label.scroll')</option>
-                        <option value="piston">@lang('messages.label.piston')</option>
-                    </x-native-select>
+                    <ul class="list-disc">
+                        <li>Agencia:</li>
+                        <li>Estado:</li>
+                        <li>Oficina:</li>
+                        <li>Piso:</li>
+                        <li>Capacidad(BTU):</li>
+                        <li>Tipo de Sistema:</li>
+                        <!-- ... -->
+                      </ul>
                 </div>
                 <div class="p-2">
-                    <label class="opacity-60 mb-1 block text-sm font-medium text-italblue">@lang('messages.label.marca')</label>
-                    <x-input icon="pencil" wire:model="marca" />
+                    <label class="font-extrabold text-black drop-shadow-lg">@lang('messages.label.qrConden')</label>
+                    {!! QrCode::size(300)->generate('123456') !!}
                 </div>
                 <div class="p-2">
-                    <label class="opacity-60 mb-1 block text-sm font-medium text-italblue">@lang('messages.label.amp')</label>
-                    <x-input icon="pencil" wire:model="amp" />
-                </div>
-                <div class="p-2">
-                    <label class="opacity-60 mb-1 block text-sm font-medium text-italblue">@lang('messages.label.placa')</label>
-                    <input id="" wire:model="doc2" type="file" class="w-full -ml-10 pl-10 pr-3 py-2 rounded-lg border-gray-200 outline-none focus:border-indigo-500 disabledDocCC">
-                </div>
+                    <label class="font-extrabold text-black drop-shadow-lg">@lang('messages.label.qrEva')</label>
+                {!! QrCode::merge(asset('images/check_logo.png'), 0.5, true)
+                            ->size(300)->errorCorrection('H')
+                            ->generate('2020'); !!}
+            </div>
             </div>
         </div>
 
