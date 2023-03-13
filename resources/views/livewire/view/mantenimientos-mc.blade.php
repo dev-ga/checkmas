4c12acbb7efbf79bb13700c1b080caddd44b3ab6
STARKMEDIOS DEVELOPER <starkmediosdeveloper@Mac-mini-de-JHONNY.local>
2023-03-13 18:31:16 -0400

    cambio pequen

--- resources/views/livewire/view/mantenimientos-mc.blade.php
+++ resources/views/livewire/view/mantenimientos-mc.blade.php
@@ -142,7 +142,7 @@
 
                             @if(Auth::user()->rol == 5 || Auth::user()->rol == 6)
                             {{-- Costo Operacional --}}
-                            <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">{{ $item->costo_oper }}</td>
+                            <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 ">{{ $item->costo_oper }}</td>
                             @endif
 
                             {{-- Costo Cliente --}}
@@ -201,8 +201,8 @@
                                     </svg>
                                     <div class="flex items-center gap-x-2 mr-8">
                                         <div>
-                                            <h2 class="text-sm font-bold text-orange-500 dark:text-white ">Ejecuion</h2>
-                                            <p class="text-xs font-normal text-orange-300 dark:text-gray-400">Orden de trabajo en ejecucion</p>
+                                            <h2 class="text-sm font-bold text-orange-500 dark:text-white ">Ejecucion</h2>
+                                            <p class="text-xs font-normal text-orange-500 dark:text-gray-400">Orden de trabajo<br>en ejecucion</p>
                                         </div>
                                     </div>
                                 </div>
@@ -210,11 +210,16 @@
                             @endif
                             @if($item->statusOts == '4')
                             <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                <div class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-yellow-500 bg-yellow-100/60 dark:bg-gray-800">
+                                <div class="inline-flex items-center px-2 py-1 rounded-full gap-x-2 text-yellow-500 bg-yellow-100/60 dark:bg-gray-800">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                                     </svg>
-                                    <h2 class="text-sm font-normal">Supervicion</h2>
+                                    <div class="flex items-center gap-x-2 mr-8">
+                                        <div>
+                                            <h2 class="text-sm font-bold text-yellow-500 dark:text-white ">Supervicion</h2>
+                                            <p class="text-xs font-normal text-yellow-500 dark:text-gray-400">Orden de trabajo<br>esta en supervicion</p>
+                                        </div>
+                                    </div>
                                 </div>
                             </td>
                             @endif
