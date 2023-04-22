dd0c9da9dc4074fe682e9675eb663a418964752e
STARKMEDIOS DEVELOPER <starkmediosdeveloper@Mac-mini-de-JHONNY.local>
2023-04-22 13:56:29 -0400

    correcion de fondo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -29,7 +29,7 @@
         <x-notifications />
         <x-notificaciones />
 
-        <div class="font-sans antialiased  bg-check-blue h-screen">
+        <div class="font-sans antialiased h-screen">
             {{ $slot }}
         </div>
         @stack('modals')
