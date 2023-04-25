9afe484ba60208f2dc4cd1f507e457d69dc2da38
STARKMEDIOS DEVELOPER <starkmediosdeveloper@Mac-mini-de-JHONNY.local>
2023-04-25 13:33:29 -0400

    personalizacion de funcion para cambio de estatus, y modificacion de lista de Ots en la bitacora

--- resources/views/livewire/view/mantenimientos.blade.php
+++ resources/views/livewire/view/mantenimientos.blade.php
@@ -205,13 +205,16 @@
                                           </svg>                                      
                                     </button>
 
+
                                     {{-- Resetear status --}}
+                                    @if(Auth::user()->email == 'richardmaclaud@hotmail.com')
                                     <button type="submit" wire:click="resetearStatus({{ $item->id }}, '5')" class="flex ml-9 mt-2 items-end text-gray-400 transition-colors duration-200 font-extrabold  dark:hover:text-indigo-500 dark:text-gray-300 hover:text-green-500 focus:outline-none">
                                         <span class="text-xs">Resetar estatus</span>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="text-orange-500 ml-3 w-4 h-4">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                                           </svg>                                          
                                     </button>
+                                    @endif
                                     
                                 </td>
                                 @else
