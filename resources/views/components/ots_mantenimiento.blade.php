a8adcf59a8f59ab0e82fb06a449bc1019c12c0ee
STARKMEDIOS DEVELOPER <starkmediosdeveloper@Mac-mini-de-JHONNY.local>
2023-04-26 15:55:02 -0400

    Cambio que permite a los supervisores ver las Ots en la Bitacora

--- resources/views/components/ots_mantenimiento.blade.php
+++ resources/views/components/ots_mantenimiento.blade.php
@@ -1,13 +1,24 @@
 @php
 use App\Models\Ot;
+$user = Auth::user()->rol;
+dd($user);
+$ots_supervisor = Ot::all();
 $ots = Ot::where('statusOts', '3')
             ->where('tecRes_email', Auth::user()->email)
-            ->orWhere('owner', Auth::user()->email)
             ->get();
 @endphp
-<x-native-select wire:model="nro_ot" class="focus:ring-check-blue focus:border-check-blue">
-    <option value="">...</option>
-        @foreach($ots as $item)
-            <option value="{{ $item->otUid }}">{{ $item->otUid }}</option>
-        @endforeach
-</x-native-select>
+@if ($user == '7')
+    <x-native-select wire:model="nro_ot" class="focus:ring-check-blue focus:border-check-blue">
+        <option value="">...</option>
+            @foreach($ots_supervisor as $item)
+                <option value="{{ $item->otUid }}">{{ $item->otUid }}</option>
+            @endforeach
+    </x-native-select>
+@else
+    <x-native-select wire:model="nro_ot" class="focus:ring-check-blue focus:border-check-blue">
+        <option value="">...</option>
+            @foreach($ots as $item)
+                <option value="{{ $item->otUid }}">{{ $item->otUid }}</option>
+            @endforeach
+    </x-native-select> 
+@endif
