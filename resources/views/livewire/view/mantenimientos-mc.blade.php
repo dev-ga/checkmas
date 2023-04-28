2b22f1f76acf91c4319a711c77e2533476c0ca07
STARKMEDIOS DEVELOPER <starkmediosdeveloper@Mac-mini-de-JHONNY.local>
2023-04-28 08:57:59 -0400

    Correciones para mejoras en vista mobile

--- resources/views/livewire/view/mantenimientos-mc.blade.php
+++ resources/views/livewire/view/mantenimientos-mc.blade.php
@@ -323,14 +323,14 @@
                     @if($item->statusOts == '1')
                     @if(Auth::user()->rol == 5)
                     <div wire:click="updateStatusAdmin({{ $item->id }}, '2')" class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-sky-500 bg-sky-100/60 dark:bg-gray-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                         <h2 class="text-sm font-normal">@lang('messages.estatus.creada')</h2>
                     </div>
                     @else
                     <div class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-sky-500 bg-sky-100/60 dark:bg-gray-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
                         <h2 class="text-sm font-normal">@lang('messages.estatus.creada')</h2>
@@ -341,7 +341,7 @@
                     @if($item->statusOts == '2')
 
                     <div class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-green-500 bg-green-100/60 dark:bg-gray-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" />
                         </svg>
                         <h2 class="text-sm font-normal">@lang('messages.estatus.aprobada')</h2>
@@ -352,10 +352,10 @@
                     @if($item->statusOts == '3')
 
                     <div class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-orange-500 bg-orange-100/60 dark:bg-gray-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z" />
                         </svg>
-                        <div class="flex items-center gap-x-2 mr-8">
+                        <div class="flex items-center gap-x-2">
                             <div>
                                 <h2 class="text-sm font-bold text-orange-500 dark:text-white ">@lang('messages.estatus.ejecucion')</h2>
                             </div>
@@ -367,10 +367,10 @@
                     @if($item->statusOts == '4')
 
                     <div class="inline-flex items-center px-2 py-1 rounded-full gap-x-2 text-yellow-500 bg-yellow-100/60 dark:bg-gray-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                         </svg>
-                        <div class="flex items-center gap-x-2 mr-8">
+                        <div class="flex items-center gap-x-2">
                             <div>
                                 <h2 class="text-sm font-bold text-yellow-500 dark:text-white ">@lang('messages.estatus.supervicion')</h2>
                             </div>
@@ -382,7 +382,7 @@
                     @if($item->statusOts == '5')
 
                     <div class="inline-flex items-center font-bold px-3 py-1 rounded-full gap-x-2 text-green-700 bg-green-100/60 dark:bg-gray-800">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
                         </svg>
                         <h2 class="text-sm font-normal">@lang('messages.estatus.finalizada')</h2>
@@ -395,13 +395,13 @@
                 <div>
                     @if($item->statusOts_banco == '1')
                     <div wire:click="updateStatusBanco({{ $item->id }}, '1')" class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-orange-500 bg-orange-100/60 dark:bg-gray-800 cursor-pointer">
-                        <img class="object-cover ml-2 my-2 w-auto h-6" src="{{ asset('images/DEL-TESORO-COLOR.png') }}" alt="">
+                        <img class="object-cover ml-2 my-2 w-auto h-4" src="{{ asset('images/DEL-TESORO-COLOR.png') }}" alt="">
                         <h2 class="text-sm font-normal">@lang('messages.estatus.poraprobacion')</h2>
                     </div>
                     @endif
                     @if($item->statusOts_banco == '2')
                     <div class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-green-500 bg-green-100/60 dark:bg-gray-800">
-                        <img class="object-cover ml-2 my-2 w-auto h-6" src="{{ asset('images/DEL-TESORO-COLOR.png') }}" alt="">
+                        <img class="object-cover ml-2 my-2 w-auto h-4" src="{{ asset('images/DEL-TESORO-COLOR.png') }}" alt="">
                         <h2 class="text-sm font-normal">@lang('messages.estatus.aprobada')</h2>
                     </div>
                     @endif
