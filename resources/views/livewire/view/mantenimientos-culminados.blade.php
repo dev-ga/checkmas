5af72f31cd6dabdd81499eb2d402f5187f4c62d5
STARKMEDIOS DEVELOPER <starkmediosdeveloper@Mac-mini-de-JHONNY.local>
2023-05-03 17:16:10 -0400

    refactorizacion para vista mobile

--- resources/views/livewire/view/mantenimientos-culminados.blade.php
+++ resources/views/livewire/view/mantenimientos-culminados.blade.php
@@ -94,42 +94,211 @@
         {{-- Fin Div paginacion --}}
     </div>
         {{-- Tabla Responsive para movil --}}
-        <div class="grid grid-cols-1 sm:grid-cols-1 gap-4 md:hidden">
+        <div class="grid grid-cols-1 sm:grid-cols-1 gap-4 md:hidden rounded-lg p-1 shadow-sm shadow-indigo-100">
             @foreach ($data as $item)
+            <div class="bg-white p-4 rounded-lg shadow mb-4">
+                <a href="{{ asset('/storage/'.$item->foto1_antes) }}" target="_blank">
+                    <img src="{{ asset('/storage/'.$item->foto1_antes) }}" class="h-56 w-full rounded-md object-cover"/>
+                    <div class="mt-2">
+                        <!-- Oden de trabajo -->
+                        <dl>
+                            <div>
+                                <dd class="text-sm text-gray-500">Orden de trabajo</dd>
+                            </div>
+                            <div>
+                                <dd class="font-medium">Nro. {{ $item->nro_ot }}</dd>
+                            </div>
+                        </dl>
+                        <!-- Div contenido -->
+                        <div class="mt-6 flex items-center gap-8 text-xs">
+                            <!-- Estado -->
+                            <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-check-blue">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                </svg>
+                                <div class="mt-1.5 sm:mt-0">
+                                    <p class="text-gray-500">Estado</p>
+                                    <p class="font-medium">{{ $item->estado }}</p>
+                                </div>
+                            </div>
+                            <!-- Agencia -->
+                            <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-check-blue">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
+                                </svg>
+                                <div class="mt-1.5 sm:mt-0">
+                                    <p class="text-gray-500">Agencia</p>
+                                    <p class="font-medium">{{ $item->agencia }}</p>
+                                </div>
+                            </div>
+                            <!-- Foto -->
+                            <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-check-blue">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                                </svg>
+                                <div class="mt-1.5 sm:mt-0">
+                                    <p class="text-gray-500">Foto</p>
+                                    <p class="font-medium">Antes</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="bg-white p-4 rounded-lg shadow mb-4">
+                <a href="{{ asset('/storage/'.$item->foto2_antes) }}" target="_blank">
+                    <img src="{{ asset('/storage/'.$item->foto2_antes) }}" class="h-56 w-full rounded-md object-cover"/>
+                    <div class="mt-2">
+                        <!-- Oden de trabajo -->
+                        <dl>
+                            <div>
+                                <dd class="text-sm text-gray-500">Orden de trabajo</dd>
+                            </div>
+                            <div>
+                                <dd class="font-medium">Nro. {{ $item->nro_ot }}</dd>
+                            </div>
+                        </dl>
+                        <!-- Div contenido -->
+                        <div class="mt-6 flex items-center gap-8 text-xs">
+                            <!-- Estado -->
+                            <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-check-blue">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                </svg>
+                                <div class="mt-1.5 sm:mt-0">
+                                    <p class="text-gray-500">Estado</p>
+                                    <p class="font-medium">{{ $item->estado }}</p>
+                                </div>
+                            </div>
+                            <!-- Agencia -->
+                            <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-check-blue">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
+                                </svg>
+                                <div class="mt-1.5 sm:mt-0">
+                                    <p class="text-gray-500">Agencia</p>
+                                    <p class="font-medium">{{ $item->agencia }}</p>
+                                </div>
+                            </div>
+                            <!-- Foto -->
+                            <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-check-blue">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                                </svg>
+                                <div class="mt-1.5 sm:mt-0">
+                                    <p class="text-gray-500">Foto</p>
+                                    <p class="font-medium">Antes</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="bg-white p-4 rounded-lg shadow mb-4">
+                <a href="{{ asset('/storage/'.$item->foto1_despues) }}" target="_blank">
+                    <img src="{{ asset('/storage/'.$item->foto1_despues) }}" class="h-56 w-full rounded-md object-cover"/>
+                    <div class="mt-2">
+                        <!-- Oden de trabajo -->
+                        <dl>
+                            <div>
+                                <dd class="text-sm text-gray-500">Orden de trabajo</dd>
+                            </div>
+                            <div>
+                                <dd class="font-medium">Nro. {{ $item->nro_ot }}</dd>
+                            </div>
+                        </dl>
+                        <!-- Div contenido -->
+                        <div class="mt-6 flex items-center gap-8 text-xs">
+                            <!-- Estado -->
+                            <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-check-blue">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                </svg>
+                                <div class="mt-1.5 sm:mt-0">
+                                    <p class="text-gray-500">Estado</p>
+                                    <p class="font-medium">{{ $item->estado }}</p>
+                                </div>
+                            </div>
+                            <!-- Agencia -->
+                            <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-check-blue">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
+                                </svg>
+                                <div class="mt-1.5 sm:mt-0">
+                                    <p class="text-gray-500">Agencia</p>
+                                    <p class="font-medium">{{ $item->agencia }}</p>
+                                </div>
+                            </div>
+                            <!-- Foto -->
+                            <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-check-blue">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                                </svg>
+                                <div class="mt-1.5 sm:mt-0">
+                                    <p class="text-gray-500">Foto</p>
+                                    <p class="font-medium">Despues</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
             <div class="bg-white p-4 rounded-lg shadow">
-                <div class="mb-3">
-                    <h2 class="text-sm font-bold text-gray-800 dark:text-white ">@lang('messages.tablas.nro') {{ $item->nro_ot }}</h2>
-                </div>
-                {{-- card imagenes --}}
-                <div class="flex justify-between items-center gap-x-2">
-                    <img class="object-cover w-7 h-7 rounded-full" src="{{ asset('images/orden-de-trabajo.png') }}" alt="">
-                    <div>
-                        <p class="text-xs font-normal text-gray-600 dark:text-gray-400">Agencia: {{ $item->agencia }}</p>
-                        <p class="text-xs font-normal text-gray-600 dark:text-gray-400">Estado: {{ $item->estado }}</p>
-                    </div>
-
-                    <div>
-                        <div class="flex items-center gap-x-1 justify-end">
-                            <p class="text-xs font-normal text-gray-600 dark:text-gray-400">Antes</p>
-                            <a class="p-2" href="{{ asset('/storage/'.$item->foto1_antes) }}" target="_blank">
-                                <img src="{{ asset('/storage/'.$item->foto1_antes) }}" class="w-8 h-8" />
-                            </a>
-                            <a class="p-2" href="{{ asset('/storage/'.$item->foto2_antes) }}" target="_blank">
-                                <img src="{{ asset('/storage/'.$item->foto2_antes) }}" class="w-8 h-8" />
-                            </a>
+                <a href="{{ asset('/storage/'.$item->foto2_despues) }}" target="_blank">
+                    <img src="{{ asset('/storage/'.$item->foto2_despues) }}" class="h-56 w-full rounded-md object-cover"/>
+                    <div class="mt-2">
+                        <!-- Oden de trabajo -->
+                        <dl>
+                            <div>
+                                <dd class="text-sm text-gray-500">Orden de trabajo</dd>
+                            </div>
+                            <div>
+                                <dd class="font-medium">Nro. {{ $item->nro_ot }}</dd>
+                            </div>
+                        </dl>
+                        <!-- Div contenido -->
+                        <div class="mt-6 flex items-center gap-8 text-xs">
+                            <!-- Estado -->
+                            <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-check-blue">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                </svg>
+                                <div class="mt-1.5 sm:mt-0">
+                                    <p class="text-gray-500">Estado</p>
+                                    <p class="font-medium">{{ $item->estado }}</p>
+                                </div>
+                            </div>
+                            <!-- Agencia -->
+                            <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-check-blue">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0012 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75z" />
+                                </svg>
+                                <div class="mt-1.5 sm:mt-0">
+                                    <p class="text-gray-500">Agencia</p>
+                                    <p class="font-medium">{{ $item->agencia }}</p>
+                                </div>
+                            </div>
+                            <!-- Foto -->
+                            <div class="sm:inline-flex sm:shrink-0 sm:items-center sm:gap-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-5 w-5 text-check-blue">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                                </svg>
+                                <div class="mt-1.5 sm:mt-0">
+                                    <p class="text-gray-500">Foto</p>
+                                    <p class="font-medium">Despues</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="flex items-center gap-x-1 justify-end">
-                            <p class="text-xs font-normal text-gray-600 dark:text-gray-400">Despues</p>
-                            <a class="p-2" href="{{ asset('/storage/'.$item->foto1_despues) }}" target="_blank">
-                                <img src="{{ asset('/storage/'.$item->foto1_despues) }}" class="w-8 h-8" />
-                            </a>
-                            <a class="p-2" href="{{ asset('/storage/'.$item->foto2_despues) }}" target="_blank">
-                                <img src="{{ asset('/storage/'.$item->foto2_despues) }}" class="w-8 h-8" />
-                            </a>
-                        </div>
                     </div>
-                </div>
-
+                </a>
             </div>
             @endforeach
             <div class="bg-white px-4 py-3 items-center justify-between border-t border-gray-200 sm:px-6">
@@ -137,5 +306,6 @@
                 {{ $data->links() }}
             </div>
         </div>
+
 </div>
 
