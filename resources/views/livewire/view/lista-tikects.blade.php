ecb3f8c388ebc83e371f0bcd33dd90dc25b81d71
Gustavo <gusta.acp@gmail.com>
2023-05-27 11:06:56 -0400

    colocacion de menu de acciones en la lista de ticket

--- resources/views/livewire/view/lista-tikects.blade.php
+++ resources/views/livewire/view/lista-tikects.blade.php
@@ -29,6 +29,7 @@
                                     </button>
                                 </div>
                             </th>
+
                             <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                 <div class="flex items-center gap-x-3">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -39,12 +40,12 @@
                                         <span>@lang('messages.tablas.ubicacion')</span>
                                     </button>
                                 </div>
-                            </th>
-        
+                            </th>   
+
                             <th scope="col" class="w-20 px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                 @lang('messages.tablas.observacion')
-                            </th>
-        
+                            </th>  
+
                             <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                                 <div class="flex items-center gap-x-3">
                                     <span>@lang('messages.tablas.estatus')</span>
@@ -53,16 +54,15 @@
                                     </button>
                                 </div>
                             </th>
-        
+                            
                             <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                @lang('messages.tablas.asignar')
                             </th>
                         </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
                         @foreach ($data as $item)
                         <tr>
-                            <td class="px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
+                            <td class="w-1/4 px-4 py-4 text-sm font-medium text-gray-700 dark:text-gray-200 whitespace-nowrap">
                                 <div class="flex items-center gap-x-2">
                                     <img class="object-cover w-7 h-7 rounded-full" src="{{ asset('images/orden-de-trabajo.png') }}" alt="">
                                     <div>
@@ -74,8 +74,8 @@
                                     </div>
                                 </div>
                             </td>
-                            {{-- <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">{{ $item->tipoServicio }}</td> --}}
-                            <td class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
+
+                            <td class="w-1/4 px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
                                 <div class="flex items-center gap-x-2">
                                     <div>
                                         <p class="text-xs font-bold text-gray-600 dark:text-gray-400">@lang('messages.tablas.agencia') {{ $item->agencia }}</p>
@@ -84,7 +84,8 @@
                                 </div>
                             </td>
         
-                            <td class="w-1/4 px-4 py-4 text-sm text-gray-500 text-justify dark:text-gray-300">{{ $item->observaciones }}</td>
+                            <td class="w-1/3 px-4 py-4 text-sm text-gray-500 text-justify dark:text-gray-300">{{ $item->observaciones }}</td>
+
                             @if($item->status_tikect == '0')
                             <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
                                 <div class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-sky-500 bg-sky-100/60 dark:bg-gray-800">
@@ -95,6 +96,7 @@
                                 </div>
                             </td>
                             @endif
+
                             @if($item->status_tikect == '1')
                             <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
                                 <div class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-red-500 bg-red-100/60 dark:bg-gray-800">
@@ -105,38 +107,23 @@
                                 </div>
                             </td>
                             @endif
-                            @if($item->status_tikect == '2')
-                            <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                <div class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-orange-500 bg-orange-100/60 dark:bg-gray-800">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                    </svg>
-                                    <h2 class="text-sm font-normal">@lang('messages.estatus.anulado')</h2>
-                                </div>
+
+                            {{-- Menu --}}
+                            <td class="px-4 py-4 text-sm text-gray-500 text-justify dark:text-gray-300">
+                                <x-dropdown>
+
+                                    @if(Auth::user()->rol == 5 || Auth::user()->rol == 6)
+                                        <x-dropdown.item label="Asignar OT" wire:click="CrearOt({{ $item->id }})"/>
+                                    @endif
+
+                                    @if(Auth::user()->rol == 1 || Auth::user()->rol == 2 || Auth::user()->rol == 3)
+                                        <x-dropdown.item label="Cerrar" type="submit" wire:click="updateStatusTikect({{ $item->id }}, '1')"/>
+                                    @endif
+
+                                    <x-dropdown.item label="Eliminar" wire:click="eliminar({{ $item->id }})"/>
+
+                                </x-dropdown>
                             </td>
-                            @endif
-                            {{-- Cambio de estatus --}}
-                            @if(Auth::user()->rol == 1 || Auth::user()->rol == 2 || Auth::user()->rol == 3)
-                            <td class="px-4 py-4 text-sm whitespace-nowrap">
-        
-                                <button type="submit" wire:click="updateStatusTikect({{ $item->id }}, '1')" class="px-2 py-2 rounded-full bg-red-100/60 text-red-800 transition-colors duration-200 font-extrabold  dark:hover:text-indigo-500 dark:text-gray-300 hover:text-red-500 focus:outline-none">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-                                    </svg>
-                                </button>
-        
-                            </td>
-                            @endif
-                            @if(Auth::user()->rol == 5 || Auth::user()->rol == 6)
-                            <td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
-                                <div wire:click="CrearOt({{ $item->id }})" class="inline-flex items-center px-3 py-1 rounded-full gap-x-2 text-check-green bg-blue-100/60 dark:bg-gray-800 cursor-pointer">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
-                                    </svg>
-                                    <h2 class="text-sm font-normal">@lang('messages.tablas.asignar')</h2>
-                                </div>
-                            </td>
-                            @endif
                         </tr>
                         @endforeach
                     </tbody>
