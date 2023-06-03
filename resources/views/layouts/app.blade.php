c080f2059981c6fc5a3e6fce4b1c573312b10ebc
Gustavo <32278519+dev-ga@users.noreply.github.com>
2023-06-03 15:24:38 -0400

    Revert "modificacion de los graficos"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,20 +32,12 @@
     </head>
     <body class="font-sans antialiased">
         <x-notifications /> 
-        <x-dialog z-index="z-50" blur="md" align="center" />
 
 
             <div x-data="{ sidebarOpen: false }" class="flex flex-row min-h-screen">
-                @if(Auth::user()->empresa == 'Trx' || Auth::user()->empresa == 'Banco del Tesoro')
-                    {{-- Sidebar principal --}}
-                    <x-sidebar-principal />
-                @endif
-
-                @if(Auth::user()->empresa == 'IAIM')   
-                    {{-- Sidebar IAIM --}}
-                    <x-sidebar-iaim />
-                @endif
-
+                
+                {{-- Sidebar principal --}}
+                <x-sidebar-principal />
                 <div class="flex-1 flex flex-col overflow-hidden">
                     <x-header-ppal></x-header-ppal>
 
@@ -76,35 +68,6 @@
 
         {{-- Utils.js --}}
         <script  src="{{ asset('js/utils.js') }}" type="text/javascript"></script>
-        <script>
-            var ratonParado = null;
-            var milisegundosLimite = 600000;
-            $(document).on('mousemove', function() {
-                clearTimeout(ratonParado);
-                ratonParado = setTimeout(function() {
-                    $.ajax
-                    ({
-                        url: "{{ route('logout') }}",
-                        method: 'GET',
-                        contentType: false,
-                        cache: false,
-                        processData: false,
-                        success:function(response)
-                        {
-                            window.$wireui.dialog({
-                            title: 'Sesión inactiva!',
-                            description: 'Su sesión fue cerrada por inactividad. Debe iniciar sesión nuevamente.',
-                            icon: 'error'
-                            })
-                            console.log('cerro la sesion')
-                        },
-                        error: function(response) {
-                            console.log(response)
-                        }
-                    });
-                }, milisegundosLimite);
-            });
-        </script>
 
     </body>
 </html>
